3112492ab7205d3768f3a5b6c3878d9dcc1763a0
datnc <hipboylc@gmail.com>
2020-03-15 17:23:43 +0700

    2 files modified

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Product } from "../Product";
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  selector: "app-product",
+  templateUrl: "./product.component.html",
+  styleUrls: ["./product.component.css"]
 })
 export class ProductComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  product: Product = {
+    name: "Item 1",
+    price: 999,
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!",
+    stt: true
+  };
+  onchange(){
+    this.product.stt = false;
   }
-
 }
