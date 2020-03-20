fdf8a48417f668b8d25ef3eee1c70b4615cf8ee8
datnc <hipboylc@gmail.com>
2020-03-21 04:35:11 +0700

    2 files modified

--- src/app/MockData.ts
+++ src/app/MockData.ts
@@ -13,5 +13,61 @@ export const data :Product[] = [
     price: 999,
     desc:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!"
+  },
+     {
+    id: 3,
+    name: "Item 3",
+    price: 999,
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!"
+  },
+     {
+    id: 4,
+    name: "Item 4",
+    price: 999,
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!"
+  },
+     {
+    id: 5,
+    name: "Item 5",
+    price: 999,
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!"
+  },
+     {
+    id: 6,
+    name: "Item 6",
+    price: 999,
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!"
+  },
+     {
+    id: 7,
+    name: "Item 7",
+    price: 999,
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!"
+  },
+     {
+    id: 8,
+    name: "Item 8",
+    price: 999,
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!"
+  },
+     {
+    id: 9,
+    name: "Item 9",
+    price: 999,
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!"
+  },
+     {
+    id: 10,
+    name: "Item 10",
+    price: 999,
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!"
   }
 ];
